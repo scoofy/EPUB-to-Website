60fb6ae20a57acbd0136f1dcd3e63ae961955375
scoofy <mschoolfield@Matthews-MacBook-Pro.local>
2015-04-20 01:23:51 -0700

    author url update

--- data/data.py
+++ data/data.py
@@ -1,4 +1,4 @@
 BOOK_TITLE = "How to Bike"
 BOOK_SUBTITLE = "A Modern Guide to Urban Cycling"
 AUTHOR_NAME = "Matthew Schoolfield"
-AUTHOR_WEBSITE = "http://www.mattschoolfield.info"
+AUTHOR_WEBSITE = "http://mattschoolfield.info"
