8e04e8d189bbfbd42f64fc84364bc155c0f100bb
scoofy <mschoolfield@matthews-mbp.gateway.pace.com>
2015-04-22 14:19:00 -0700

    take out my data

--- data/data.py
+++ data/data.py
@@ -1,8 +1,8 @@
-BOOK_TITLE = "How to Bike"
-BOOK_SUBTITLE = "A Modern Guide to Urban Cycling"
-AUTHOR_NAME = "Matthew Schoolfield"
-AUTHOR_WEBSITE = "http://mattschoolfield.info"
-EPUB_FILENAME = "howtobike.epub"
+BOOK_TITLE = ""
+BOOK_SUBTITLE = ""
+AUTHOR_NAME = ""
+AUTHOR_WEBSITE = ""
+EPUB_FILENAME = ""
 
 EPUB_UPLOADED = False	# You'll change this after you upload your epub
 EPUB_FILE_URL = ""		# You'll get this after you upload your epub
