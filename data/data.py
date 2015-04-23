815f87a2a9772079bc51d5b0541cca6edcd4b513
scoofy <mschoolfield@matthews-mbp.gateway.pace.com>
2015-04-23 16:22:25 -0700

    moved comment lower for easy pasting

--- data/data.py
+++ data/data.py
@@ -5,6 +5,9 @@ AUTHOR_WEBSITE = ""
 EPUB_FILENAME = ""
 
 EPUB_UPLOADED = False	# You'll change this after you upload your epub
-EPUB_FILE_URL = ""		# You'll get this after you upload your epub
+EPUB_FILE_URL = ""
+
+# You'll get this after you upload your epub
+
 # If you need to update the version of the epub you're serving, change "EPUB_UPLOADED" to False.
 # Then, navigate to "/upload" and you'll be able to delete the old version when uploading the new one.
